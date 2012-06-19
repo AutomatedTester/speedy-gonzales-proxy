9124170bd60bdfbe6803508eac27b7287dbf4964
AutomatedTester <dburns@mozilla.com>
2012-06-20 00:23:30 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+= Speedy Gonzales Proxy =
+
+This is a project to help make Mozilla Web QA Selenium Tests more reliable by making sure the mouse is out of the way.
+
---
+++ src/com/mozilla/seleniumgrid/SpeedyGonzalesProxy.java
@@ -0,0 +1,49 @@
+package com.mozilla.seleniumgrid;
+
+import org.openqa.grid.common.RegistrationRequest;
+import org.openqa.grid.internal.Registry;
+import org.openqa.grid.internal.TestSession;
+import org.openqa.grid.internal.listeners.TestSessionListener;
+import org.openqa.grid.selenium.proxy.DefaultRemoteProxy;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SpeedyGonzalesProxy extends DefaultRemoteProxy implements TestSessionListener {
+
+    private Pattern urlPattern = Pattern.compile("http://(\\w+)");
+
+    public SpeedyGonzalesProxy(RegistrationRequest request, Registry registry) {
+        super(request, registry);
+    }
+
+    @Override
+    public void beforeSession(TestSession session) {
+        super.beforeSession(session);
+        synchronized (this){
+            try {
+                String nodeAddress = session.getSlot().getPath();
+                Matcher matcher = urlPattern.matcher(nodeAddress);
+                String cleanAddress = "";
+                while (matcher.find()){
+                    cleanAddress = matcher.group();
+                }
+                URL url = new URL("http://" + cleanAddress + ":3000/");
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.getInputStream();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
